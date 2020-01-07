6b79a22ab85addd5080b02179add0131f5c5c6fe
romulo <romulosousa@fabricainfo.com>
2020-01-07 11:43:39 -0300

    função header para engloadar o cabeçalho da app

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './component-header.css';
+
+/** Stateless Component  */
+const Header = () => (
+  <header id="main-header">Hunt</header>
+);
+
+export default Header;
