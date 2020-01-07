13a3462af10f5d9e6a93b13e822009fefcdae3b5
romulo <romulosousa@fabricainfo.com>
2020-01-07 14:27:01 -0300

    importando as routes

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './style.css';
 
 import Header from './components/Header';
-import Main from './pages/Main';
+
+import Main from './pages/Main'
+import Routes from './routes';
 
 export default class App extends Component {
   render() {
