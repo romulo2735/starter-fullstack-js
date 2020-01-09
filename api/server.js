829db28cdd0e18c701123ade32e98567df1a5f08
romulo <romulosousa@fabricainfo.com>
2020-01-09 19:19:09 -0300

    required DIR mongoose

--- api/server.js
+++ api/server.js
@@ -1,11 +1,19 @@
 const express  = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+/** Start Server */
 const app = express();
 
+/** Start Connection Database */
+mongoose.connect('mongodb://localhost/huntapi', {useNewUrlParser: true});
+requireDir('./src/models/Product');
+
+/** Routes */
 app.get('/', (req, res) => {
   res.send('Welcome to api');
 });
 
 
-app.listen(3001);
+app.listen(3000);
 
